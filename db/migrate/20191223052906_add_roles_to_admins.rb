5028f39f58e9db86d95f9576863b3cb686383d1c
itokiana <itokiana@gmail.com>
2020-01-06 08:50:21 +0300

    Modify create_admin and add_roles_to_admin migrations

--- db/migrate/20191223052906_add_roles_to_admins.rb
+++ db/migrate/20191223052906_add_roles_to_admins.rb
@@ -1,7 +1,7 @@
 class AddRolesToAdmins < ActiveRecord::Migration[6.0]
   def change
-    add_column :admins, :superadmin_role, :boolean
-    add_column :admins, :supervisor_role, :boolean
-    add_column :admins, :user_role, :boolean
+    add_column :admins, :superadmin_role, :boolean, default: false
+    add_column :admins, :supervisor_role, :boolean, default: false
+    add_column :admins, :user_role, :boolean, default: true
   end
 end
